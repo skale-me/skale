455b06fa144348df886cfa397256191875dc894c
Idoia Aranguren <idoia.aranguren@ugrid.net>
2014-12-04 18:11:13 +0100

    merge setup.js with map.js

--- test/map/map.js
+++ test/map/map.js
@@ -1,21 +1,24 @@
 #!/usr/local/bin/node --harmony
 
 var co = require('co');
-var fs = require('fs');
 var UgridClient = require('../../lib/ugrid-client.js');
 var UgridContext = require('../../lib/ugrid-context.js');
-var tl = require('../test-lib.js');
+
+var M = 5;
+var a = ml.randn(M);
+
+function doubles(n) {
+	return n * 2;
+}
 
 var grid = new UgridClient({host: 'localhost', port: 12346, data: {type: 'master'}});
-
-var json = JSON.parse(fs.readFileSync('map.json', {encoding: 'utf8'}));	
 
 co(function *() {
 	yield grid.connect();
 	var res = yield grid.send('devices', {type: "worker"});
 	var ugrid = new UgridContext(grid, res.devices);
-
-	var res = yield ugrid.parallelize(json.input).map(tl.doubles, []).collect();
+	
+	var res = yield ugrid.parallelize(a).map(doubles, []).collect();
 
 	console.log('distributed map result')
 	console.log(res);
