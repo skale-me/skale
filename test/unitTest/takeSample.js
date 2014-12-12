d32d54b57bb79d21bf960ef1b8f16b50cf7fa068
Cedric Artigue <artigue.cedric@gmail.com>
2014-12-12 23:09:17 +0100

    Fixed issue #4

---
+++ test/unitTest/takeSample.js
@@ -0,0 +1,30 @@
+#!/usr/local/bin/node --harmony
+
+var co = require('co');
+var UgridClient = require('../../lib/ugrid-client.js');
+var UgridContext = require('../../lib/ugrid-context.js');
+var ml = require('../../lib/ugrid-ml.js');
+
+var grid = new UgridClient({host: 'localhost', port: 12346, data: {type: 'master'}});
+
+co(function *() {
+	yield grid.connect();
+	var devices = yield grid.send({cmd: 'devices', data: {type: "worker"}});
+	var ugrid = new UgridContext(grid, devices);
+
+	var N = 10;
+	var n = 3;
+	var V = [];
+
+	for (var i = 0; i < N; i++) 
+		V[i] = i;
+
+	var d1 = yield ugrid.parallelize(V).takeSample(n);
+
+	console.error(V);
+	console.error();
+	console.error(d1);
+
+	grid.disconnect();
+})();
+
