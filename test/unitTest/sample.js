e40963ca3b63136ce3a3e21661de3f7e3f381eec
Cedric Artigue <artigue.cedric@gmail.com>
2014-12-12 22:16:47 +0100

    Implemented feature #11, UgridArray.sample(frac)

---
+++ test/unitTest/sample.js
@@ -0,0 +1,31 @@
+#!/usr/local/bin/node --harmony
+
+var co = require('co');
+var UgridClient = require('../../lib/ugrid-client.js');
+var UgridContext = require('../../lib/ugrid-context.js');
+var ml = require('../../lib/ugrid-ml.js');
+
+var grid = new UgridClient({host: 'localhost', port: 12346, data: {type: 'master'}});
+
+co(function *() {
+	yield grid.connect();
+	var devices = yield grid.send({cmd: 'devices', data: {type: "worker"}});
+	var ugrid = new UgridContext(grid, devices);
+
+	var N = 10;
+	var n = 2;
+	var frac = n / N;
+	var V = [];
+
+	for (var i = 0; i < N; i++) 
+		V[i] = i;
+
+	var d1 = yield ugrid.parallelize(V).sample(frac);
+
+	console.error(V);
+	console.error();
+	console.error(d1);
+
+	grid.disconnect();
+})();
+
