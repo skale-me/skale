9d9c3e729d9e651868f0d3e367db425bdc0426f3
Cedric Artigue <artigue.cedric@gmail.com>
2014-12-14 00:34:30 +0100

    fixed issue #13, collect handles multiple partitions

--- examples/debug-kmeans.js
+++ examples/debug-kmeans.js
@@ -1,41 +1,36 @@
 #!/usr/local/bin/node --harmony
-
 var co = require('co');
 var UgridClient = require('../lib/ugrid-client.js');
 var UgridContext = require('../lib/ugrid-context.js');
 var ml = require('../lib/ugrid-ml.js');
 
 var grid = new UgridClient({host: 'localhost', port: 12346, data: {type: 'master'}});
-
 co(function *() {
 	yield grid.connect();
 	var devices = yield grid.send({cmd: 'devices', data: {type: 'worker'}});
 	var ugrid = new UgridContext(grid, devices);
 
-	// Bug on initial kmeans with 4 workers with this setup
-	var N = 4, D = 16, K = 4;
-
-	// This setup is ok for a few workers but exhibits the 
-	// bug about worker's task synchronized start with number of workers > 10
-	// var N = 100, D = 2, K = 4;
-	
-	var ITERATIONS = 20;				// Number of iterations
-	var time = [];
-
+	// Bug on initial kmeans with 1 workers with this setup
+	var N = 100, D = 4, K = 2, ITERATIONS = 100;
 	var points = ugrid.loadTestData(N, D).persist();
 	var means = yield points.takeSample(K);
-	for (i = 0; i < K; i++)
+	for (var i = 0; i < K; i++) 
 		means[i] = means[i].features;
-
-	// Display input data
-	console.log('\nInitial K-means');
+	// Verify unicity of initial means
+	for (var i = 0; i < K; i++) {
+		if (means.lastIndexOf(means[i]) != i)
+			throw 'Initial means must be distinct'		
+	}
+	console.log('\nInitial K-means');	
 	console.log(means);
+	// console.log('D = ' + means[0].length);
 	// var data = yield points.collect();
 	// console.log('\nData :');
 	// console.log(data);
 
+	// BUG si on collect juste après le reduceByKey car le stage 1 est vide
+
 	for (var i = 0; i < ITERATIONS; i++) {
-		var startTime = new Date();
 		var newMeans = yield points.map(ml.closestSpectralNorm, [means])
 			.reduceByKey('cluster', ml.accumulate, {acc: ml.zeros(D), sum: 0})
 			.map(function(a) {
@@ -45,24 +40,19 @@ co(function *() {
 				return res;
 			}, [])
 			.collect();
-		var endTime = new Date();
-		time[i] = (endTime - startTime) / 1000;
-		console.log('\nIteration : ' + i + ', Time : ' + time[i]);
-		console.log(newMeans);
 		// Compare current K-means with previous iteration ones
+		console.log('\nnewMeans')
+		console.log(newMeans)
 		var dist = 0;
 		for (var k = 0; k < K; k++)
 			for (var j = 0; j < means[k].length; j++)
 				dist += Math.pow(newMeans[k][j] - means[k][j], 2);
-		console.log('squared distance : ' + dist);
 		means = newMeans;
-		if (dist < 0.01)
-			break;		
-	}
-	console.log('\nFirst iteration : ' + time[0]);
-	if (time.length > 1) {
-		time.shift();
-		console.log('Later iterations : ' + time.reduce(function(a, b) {return a + b}) / (ITERATIONS - 1));
+		console.log('\nIteration : ' + i);
+		// console.log(newMeans);
+		console.log('squared distance : ' + dist);		
+		if (dist < 0.001)
+			break;
 	}
 	grid.disconnect();
 })();
