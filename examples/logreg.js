738c009d64ee2c5d314466b939a204527dcd991a
Cedric Artigue <artigue.cedric@gmail.com>
2014-12-15 16:39:41 +0100

    fixed issue #6, added randomSVMData method to ease future benchmark

--- examples/logreg.js
+++ examples/logreg.js
@@ -12,14 +12,16 @@ co(function *() {
 	var devices = yield grid.send({cmd: 'devices', data: {type: "worker"}});
 	var ugrid = new UgridContext(grid, devices);
 
-	var N = 203472;						// Number of observations
+	var N = 203472 * 4;						// Number of observations
 	var D = 16;							// Number of features
 	var P = 4;							// Number of partitions
+	var seed = 1;
 	var ITERATIONS = 20;				// Number of iterations
 	var time = new Array(ITERATIONS);
 	var rng = new ml.Random();
 	var w = rng.randn(D);
-	var points = ugrid.loadTestData(N, D, P).persist();
+
+	var points = ugrid.randomSVMData(N, D, seed, P).persist();
 
 	for (var i = 0; i < ITERATIONS; i++) {
 		var startTime = new Date();
