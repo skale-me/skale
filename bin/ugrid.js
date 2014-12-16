e26c1d8ea8f87d5b1e4113c38a1230169f650c00
Marc Vertes <marc.vertes@ugrid.net>
2014-12-16 18:38:10 +0100

    preliminary work for fault tolerant ugrid server as per #23

--- bin/ugrid.js
+++ bin/ugrid.js
@@ -5,17 +5,34 @@
 var net = require('net');
 var uuidGen = require('node-uuid');
 var ugridMsg = require('../lib/ugrid-msg.js');
+var UgridClient = require('../lib/ugrid-client.js');
+
+var opt = require('node-getopt').create([
+	['h', 'help', 'print this help text'],
+	['H', 'Host=ARG', 'primary server host (default none)'],
+	['n', 'name=ARG', 'advertised server name (default localhost)'],
+	['P', 'Port=ARG', 'primary server port (default none)'],
+	['p', 'port=ARG', 'server port (default 12346)'],
+	['v', 'version', 'print version']
+]).bindHelp().parseSystem();
 
 var clients = {};
 var tsocks = [null];
 var clientMax = 1;
-var port = process.argv[2] || 12346;
-var c = 0;  // counter
+var name = opt.options.name || 'localhost';
+var port = opt.options.port || 12346;
+var primary = {host: opt.options.Host, port: opt.options.Port || 12346};
+var secondary;
+var isBackup = primary.host != undefined;
+var msgCount = 0;
+var cli;
+
 var client_command = {
 	connect: function (sock, msg) {
 		msg.cmd = 'reply';
 		msg.data = register(null, msg.data, sock);
 		sock.write(ugridMsg.encode(msg));
+		sock.write(ugridMsg.encode({cmd: 'broadcast', data: {secondary: secondary || {}}}));
 		console.log('Connect ' + sock.client.data.type + ' ' +
 					sock.client.index + ': ' + sock.client.uuid);
 	},
@@ -23,8 +40,21 @@ var client_command = {
 		msg.cmd = 'reply';
 		msg.data = devices(msg.data);
 		sock.write(ugridMsg.encode(msg));
-	}
+	},
+	broadcast: broadcast
 };
+
+if (isBackup) {
+	cli = new UgridClient({host: primary.host, port: primary.port, data: {type: 'backup'}});
+	cli.connect_cb(function () {
+		console.log('connected as backup to ' + primary.host + ':' + primary.port);
+		cli.send_cb({cmd: 'broadcast', data: {secondary: {host: name, port: port}}});
+		cli.on('end', function () {
+			console.log('primary server connection closed');
+		});
+		cli.on('broadcast', function(o) {});	// ignore self broadcast
+	});
+}
 
 net.createServer(function (sock) {
 	var decoder = ugridMsg.Decoder();
@@ -32,8 +62,8 @@ net.createServer(function (sock) {
 
 	decoder.on('Message', function (to, len, data) {
 		try {
-			c++;
-			if (to === 0) {
+			msgCount++;
+			if (!to) {
 				var o = JSON.parse(data.slice(8));
 				if (!(o.cmd in client_command))
                     throw 'Invalid command: ' + o.cmd;
@@ -88,7 +118,18 @@ function devices(query) {
 	return result;
 }
 
+function broadcast(sock, msg) {
+	if ('secondary' in msg.data)
+		secondary = msg.data.secondary;
+	var d = ugridMsg.encode(msg);
+	for (var i in tsocks) {
+		if (tsocks[i]) {
+			tsocks[i].write(d);
+		}
+	}
+}
+
 setInterval(function() {
-	console.log('c: ' + (c / 5) + ' msg/s');
-	c = 0;
+	console.log('msg: ' + (msgCount / 5) + ' msg/s');
+	msgCount = 0;
 }, 10000);
