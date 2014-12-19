475564b1acb06832a595f0bc0ba379990066482c
Cedric Artigue <artigue.cedric@gmail.com>
2014-12-19 11:28:30 +0100

    Fixed issue #15, sampling  is deterministic

--- test/unitTest/takeSample.js
+++ test/unitTest/takeSample.js
@@ -8,21 +8,21 @@ co(function *() {
 
 	var N = 10;
 	var n = 2;
-	var frac = n / N;
 	var seed = 1;
 	var V = [];
 
 	for (var i = 0; i < N; i++) 
 		V[i] = i;
 
-	var d1 = yield ugrid.parallelize(V).sample(frac, seed);
+	var d1 = yield ugrid.parallelize(V).takeSample(n, seed);
 
 	if (d1.length != n)
 		throw 'error: bad number of elements returned'
 
-	for ( var i = 0; i < d1.length; i++)
+	for (var i = 0; i < d1.length; i++)
 		if ( V.indexOf(d1[i]) == -1)
 			throw 'error: sampled data is not in array'
 
 	ugrid.end();
 })();
+
