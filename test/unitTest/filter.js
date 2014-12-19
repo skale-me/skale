94e46bf2149b2d89661c7f77cfeed9faf4b1ba43
Cedric Artigue <artigue.cedric@gmail.com>
2014-12-19 11:59:42 +0100

    Added filter feature as per #30

---
+++ test/unitTest/filter.js
@@ -0,0 +1,27 @@
+#!/usr/local/bin/node --harmony
+
+var co = require('co');
+var ugrid = require('../../lib/ugrid-context.js')({host: 'localhost', port: 12346});
+var ml = require('../../lib/ugrid-ml.js');
+
+co(function *() {
+	yield ugrid.init();
+
+	function positive(n) {return (n > 0) ? true : false;}
+
+	var rng = new ml.Random();
+	var N = 10;
+	var a = rng.randn(N);
+	var b = a.filter(positive);
+	
+	var dist = yield ugrid.parallelize(a).filter(positive).collect();
+
+	if (dist.length != b.length)
+		throw 'error: local and distributed array have different lengths';
+
+	for (var i = 0; i < b.length; i++)
+		if (b[i] != dist[i])
+			throw 'error: local and distributed array have different elements';
+
+	ugrid.end();
+})();
