ee34d9ff6afe9c2184bf16938d8a47fdc94a62b2
Cedric Artigue <artigue.cedric@gmail.com>
2015-01-14 15:27:07 +0100

    benchmark directory cleanup
    Former-commit-id: da26a389a6a5cb007ebb2bcc932562fdcec30619

--- benchmark/run_spark.js
+++ benchmark/run_spark.js
@@ -1,37 +1,71 @@
-#! /usr/local/bin/node
+#! /usr/local/bin/node --harmony
 
-var exec = require('child_process').exec;
+var co = require('co');
+var fs = require('fs');
+var thunkify = require('thunkify');
+var execCbk = require('child_process').exec;
+
+var exec = thunkify(execCbk);
+
+if (process.argv.length != 5) {
+	console.log('Usage: run_spark.js sparkHomeDir dataFile nIterations\n\t==> You must provide absolute path')
+	process.exit(1);
+}
 
 var SPARK_HOME = process.argv[2];
-if (process.argv[2] == undefined)
-	throw 'You must provide SPARK_HOME directory path'
+var SOURCE_DATA_FILE = process.argv[3];
+var N_ITERATIONS = process.argv[4];
 
-var EXAMPLE_DIR = SPARK_HOME + '/examples/src/main/python/mllib/';
 var IP = '127.0.0.1';
 var PORT = '7077';
-var FILE = 'logistic_regression_optimise.py';
-var SOURCE_DATA_FILE = '/gs/ugrid/work/x86_64/data.txt';
-var LOCAL_DATA_FILE = '/tmp/data.txt';
-var N_ITERATIONS = process.argv[3] || 1;
+var BIN = 'logistic_regression_optimise.py';
+// var BIN = 'kmeans.py';
+var TMP_FILE = '/tmp/' + require('path').basename(SOURCE_DATA_FILE);
+var WORKER_INSTANCES = 1;
+var MAX_CORES = 4;
 
-// Edit slaves ip in spark cluster config file
-var cmd = 'echo ' + IP + ' > ' + SPARK_HOME + '/conf/slaves';
-exec(cmd, function(error, stdout, stderr) {
-	// Copy data file to /tmp directory only if needed
-	var cmd = 'if [ ! -f ' + LOCAL_DATA_FILE + ' ];then cp ' + SOURCE_DATA_FILE + ' ' + LOCAL_DATA_FILE + '; fi';
-	exec(cmd, function(error, stdout, stderr) {
-		// Start Spark master and workers
-		var cmd = SPARK_HOME + '/sbin/start-all.sh';
-		exec(cmd, function(error, stdout, stderr) {
-			var cmd = SPARK_HOME + '/bin/spark-submit --master spark://' + IP + ':' + PORT + ' ' + EXAMPLE_DIR + FILE + ' ' + LOCAL_DATA_FILE + ' ' + N_ITERATIONS + ' 2> /dev/null';
-			var startTime = new Date();
-			exec(cmd, function(error, stdout, stderr) {
-				var elapsedTime = (new Date() - startTime) / 1000;
-				console.log('Weights: ' + stdout);
-				console.log('Elapsed Time : ' + elapsedTime);
-				var cmd = SPARK_HOME + '/sbin/stop-all.sh';
-				exec(cmd, function(error, stdout, stderr) {});
-			});
-		});
-	});
-});
+var cp_cmd = 'if [ ! -f ' + TMP_FILE + ' ];then cp ' + SOURCE_DATA_FILE + ' ' + TMP_FILE + '; fi';
+var exec_cmd = SPARK_HOME + '/bin/spark-submit --master spark://' + IP + ':' + PORT + ' ' + 
+	SPARK_HOME + '/examples/src/main/python/mllib/' + BIN + ' ' + TMP_FILE + ' ' + N_ITERATIONS + ' 2> /dev/null';
+
+co(function *() {
+	console.log('Spark home : ' + SPARK_HOME);
+	console.log('Binary file : ' + SPARK_HOME + '/examples/src/main/python/mllib/' + BIN);
+	console.log('Data file : ' + SOURCE_DATA_FILE);
+	console.log('Local data file : ' + TMP_FILE);
+	console.log('Iterations : ' + N_ITERATIONS);
+	// Copy file if needed
+	yield exec(cp_cmd);
+	
+	for (var i = 1; i <= MAX_CORES; i++) {
+		console.log('\nNumber of cores : ' + i);
+		console.log('Write spark-env.sh');
+		var spark_env = '#!/usr/bin/env bash\n\n' + 
+			'export SPARK_MASTER_IP=' + IP + '\n' + 
+			'export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64\n' +
+			'export SPARK_EXECUTOR_MEMORY=1G\n' +			
+			'export SPARK_WORKER_CORES=' + i + '\n' + 
+			'export SPARK_WORKER_INSTANCES=' + WORKER_INSTANCES + '\n';
+
+		fs.writeFileSync(SPARK_HOME + '/conf/spark-env.sh', spark_env, {encoding: 'utf8'}, function(err, res) {
+			if (err) throw 'Cannot write spark-env.sh file';
+		})
+
+		console.log('Write spark slaves file');
+		yield exec('echo ' + IP + ' > ' + SPARK_HOME + '/conf/slaves');
+
+		console.log('Start Spark cluster');
+		yield exec(SPARK_HOME + '/sbin/start-all.sh');
+
+		console.log('Run Binary');
+		var startTime = new Date();
+		var res = yield exec(exec_cmd);
+		console.log('Elapsed Time : ' + ((new Date() - startTime) / 1000));
+		console.log('Output: ' + res[0]);
+
+		console.log('Stop Spark cluster');
+		yield exec(SPARK_HOME + '/sbin/stop-all.sh');
+
+		yield exec('sleep 3');
+	}
+})();
--- benchmark/run_ugrid.js
+++ benchmark/run_ugrid.js
@@ -1,31 +1,56 @@
-#! /usr/local/bin/node
+#! /usr/local/bin/node --harmony
 
-var exec = require('child_process').exec;
+var co = require('co');
+var thunkify = require('thunkify');
+var execCbk = require('child_process').exec;
+
+var exec = thunkify(execCbk);
+
+if (process.argv.length != 5) {
+	console.log('Usage: run_ugrid.js ugridHomeDir dataFile nIterations\n\t==> You must provide absolute path')
+	process.exit(1);
+}
 
 var UGRID_HOME = process.argv[2];
-if (process.argv[2] == undefined)
-	throw 'You must provide UGRID_HOME directory path'
+var SOURCE_DATA_FILE = process.argv[3];
+var N_ITERATIONS = process.argv[4];
 
-var EXAMPLE_DIR = UGRID_HOME + '/benchmark/';
-var FILE = 'logreg-textFile.js';
-var SOURCE_DATA_FILE = '/gs/ugrid/work/x86_64/data.txt';
-var LOCAL_DATA_FILE = '/tmp/data.txt';
-var N_ITERATIONS = process.argv[3] || 1;
+var BIN = 'logreg-textFile.js';
+var TMP_FILE = '/tmp/' + require('path').basename(SOURCE_DATA_FILE);
+var MAX_CORES = 4;
 
-// Copy data file to /tmp directory only if needed
-var cmd = 'if [ ! -f ' + LOCAL_DATA_FILE + ' ];then cp ' + SOURCE_DATA_FILE + ' ' + LOCAL_DATA_FILE + '; fi';
-exec(cmd, function(error, stdout, stderr) {
-	// Start Spark master and workers
-	var cmd = UGRID_HOME + '/bin/start-all.sh ' + UGRID_HOME + '/bin';
-	exec(cmd, function(error, stdout, stderr) {
-		var cmd = EXAMPLE_DIR + FILE + ' ' + LOCAL_DATA_FILE + ' ' + N_ITERATIONS + ' 2> /dev/null';
+var cp_cmd = 'if [ ! -f ' + TMP_FILE + ' ];then cp ' + SOURCE_DATA_FILE + ' ' + TMP_FILE + '; fi';
+var exec_cmd = UGRID_HOME + '/examples/' + BIN + ' ' + TMP_FILE + ' ' + N_ITERATIONS + ' 2> /dev/null';
+
+co(function *() {
+	console.log('Ugrid home : ' + UGRID_HOME);	
+	console.log('Binary file : ' + UGRID_HOME + 'examples/' + BIN);
+	console.log('Data file : ' + SOURCE_DATA_FILE);
+	console.log('Local data file : ' + TMP_FILE);
+	console.log('Iterations : ' + N_ITERATIONS);
+	// Copy file if needed
+	yield exec(cp_cmd);
+
+	// Loop over number of cores
+	for (var i = 1; i <= MAX_CORES; i++) {
+
+		var cache_cmd = 'sh -c "echo 3 >/proc/sys/vm/drop_caches"';
+		yield exec(cache_cmd);
+
+		console.log('\nNumber of cores : ' + i);
+		console.log('Start Ugrid cluster');
+		var start_cmd = UGRID_HOME + '/bin/start-all.sh ' + UGRID_HOME + '/bin ' + i;
+		yield exec(start_cmd);
+
+		console.log('Run binary');
 		var startTime = new Date();
-		exec(cmd, function(error, stdout, stderr) {
-			var elapsedTime = (new Date() - startTime) / 1000;
-			console.log('Weights: ' + stdout);
-			console.log('Elapsed Time : ' + elapsedTime);
-			var cmd = UGRID_HOME + '/bin/stop-all.sh';
-			exec(cmd, function(error, stdout, stderr) {});
-		});
-	});
-});
+		var res = yield exec(exec_cmd);
+		console.log('Elapsed Time : ' + ((new Date() - startTime) / 1000));
+		console.log('Output: ' + res[0]);
+
+		console.log('Stop Ugrid Cluster');
+		yield exec(UGRID_HOME + '/bin/stop-all.sh');
+
+		yield exec('sleep 3');
+	}
+})();
