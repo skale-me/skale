8d30b05e1d8fffcff6d17b4e00df3a00b4bd88ce
Cedric Artigue <artigue.cedric@gmail.com>
2015-01-15 18:27:14 +0100

    Improved SVM data generation for benchmark
    Former-commit-id: 886cb5eff34d7f8885ba8ba69e7a65dd19278384

--- benchmark/gen_data.js
+++ benchmark/gen_data.js
@@ -3,19 +3,30 @@
 var fs = require('fs');
 var ml = require('../lib/ugrid-ml.js');
 
-N = 800000 * 4; //observations
-D = 16; //features 
-
-var rng = new ml.Random();
-//~ var file = '/tmp/data.txt';
-var file = '/tmp/data3.txt';
-
-var fd = fs.createWriteStream(file);
-
-for (var i = 0; i < N; i++){
-	var line = 2 * Math.round(Math.abs(rng.randn(1))) - 1;
-	line += ' ' + rng.randn(D).join(' ') + '\n';
-	fd.write(line);
+if (process.argv.length != 4) {
+	console.log('Usage: gen_data.js file size_in_Mo')
+	process.exit(1);
 }
 
-fd.end();
+var file = process.argv[2];
+var maxSize = process.argv[3] * 1024 * 1024;
+
+var rng = new ml.Random();
+var fd = fs.createWriteStream(file);
+var fileSize = 0;
+var D = 16;
+
+function writeChunk() {
+	var line = '';
+	for (var i = 0; i < 500; i++) {
+		line += 2 * Math.round(Math.abs(rng.randn(1))) - 1;
+		line += ' ' + rng.randn(D).join(' ') + '\n';
+	}
+	var lineSize = Buffer.byteLength(line, 'utf8');
+	if ((fileSize + lineSize) > maxSize)
+		fd.end();
+	else
+		fd.write(line, function(err) {fileSize += lineSize; writeChunk();});
+}
+
+writeChunk();
