11600d4f2f869ba9310d9f9c97eab949fb9274d1
Marc Vertes <marc.vertes@ugrid.net>
2015-01-20 15:11:41 +0100

    added word count example using mongodb
    Former-commit-id: 7e850b6fb8b2fc85a87cb6661e6dde27dafd6fb6

--- examples/wc-mongo.js
+++ examples/wc-mongo.js
@@ -4,7 +4,9 @@
 
 'use strict';
 
+var assert = require('assert');
 var grid = require('../lib/ugrid-context.js')();
+var MongoClient = require('mongodb').MongoClient;
 
 var file = process.argv[2] || '/etc/hosts';
 
@@ -42,22 +44,35 @@ function WorkerTask(grid, fs, readline, ml, STAGE_RAM, RAM, msg) {
 }
 
 var words = {}, finished = 0;
-grid.init_cb(function () {
-	for (var i = 0; i < grid.worker.length; i++) {
-		grid.worker[i].rpc('setTask',  {task: WorkerTask.toString(), file: file, rank: i, wmax: grid.worker.length});
-		grid.worker[i].rpc('runTask');
-	}
-	grid.on('line', function (msg) {
-		for (var i in msg.data) {
-			if (!words[i])
-				words[i] = msg.data[i];
-			else
-				words[i] += msg.data[i];
+
+MongoClient.connect('mongodb://localhost:27017/test', function (err, db) {
+	assert.equal(null, err);
+	grid.init_cb(function () {
+		var task = {
+			task: WorkerTask.toString(),
+			file: file,
+			wmax: grid.worker.length
+		};
+		var dwords = db.collection('words');
+		for (var i = 0; i < grid.worker.length; i++) {
+			task.rank = i;
+			grid.worker[i].rpc('setTask',  task);
+			grid.worker[i].rpc('runTask');
 		}
-	});
-	grid.on('end', function (msg) {
-		if (++finished < grid.worker.length) return;
-		console.log(words);
-		process.exit(0);
+		grid.on('line', function (msg) {
+			for (var i in msg.data) {
+				//if (!words[i])
+				//	words[i] = msg.data[i];
+				//else
+				//	words[i] += msg.data[i];
+				dwords.update({name: i}, {$inc: {count: msg.data[i]}}, {upsert: true, safe: false}, function () {});
+			}
+		});
+		grid.on('end', function (msg) {
+			if (++finished < grid.worker.length) return;
+			//console.log(words);
+			db.close();
+			process.exit(0);
+		});
 	});
 });
