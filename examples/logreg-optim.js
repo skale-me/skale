a92d6a858dbd9c96ded134026343e9a63e174f56
Cédric Artigue <cedric.artigue@ugrid.net>
2015-02-18 11:35:47 +0100

    Optimized svm data structure
    Former-commit-id: 6a23391e59bd3316e9dd4cae9f4a3636d40378a9

---
+++ examples/logreg-optim.js
@@ -0,0 +1,42 @@
+#!/usr/local/bin/node --harmony
+'use strict';
+
+var co = require('co');
+var ugrid = require('../lib/ugrid-context.js')();
+var ml = require('../lib/ugrid-ml.js');
+
+co(function *() {
+	yield ugrid.init();
+
+	var N = 203472 * 4;					// Number of observations
+	var D = 16;							// Number of features
+	var P = 4;							// Number of partitions
+	var seed = 1;
+	var ITERATIONS = 20;				// Number of iterations
+	var time = new Array(ITERATIONS);
+	var rng = new ml.Random();
+	var w = rng.randn(D);
+
+	var points = ugrid.randomSVMData(N, D, seed, P).persist();
+
+	// var gradient = points.map(ml.logisticLossGradient, [w]);
+
+	// console.log(yield gradient.collect());
+
+	for (var i = 0; i < ITERATIONS; i++) {
+		var startTime = new Date();
+		var gradient = yield points.map(ml.logisticLossGradient, [w]).reduce(ml.sum, ml.zeros(D));
+		for (var j = 0; j < w.length; j++)
+			w[j] -= gradient[j];
+		var endTime = new Date();
+		time[i] = (endTime - startTime) / 1000;
+		startTime = endTime;
+		console.log('\nIteration : ' + i + ', Time : ' + time[i]);
+	}
+	console.log(w);
+	console.log('\nFirst iteration : ' + time[0]);
+	time.shift();
+	console.log('Later iterations : ' + time.reduce(function (a, b) {return a + b;}) / (ITERATIONS - 1));
+
+	ugrid.end();
+})();
--- test/unitTest/randomSVMData.js
+++ test/unitTest/randomSVMData.js
@@ -13,28 +13,10 @@ co(function *() {
 	var seed = 1;
 	var res = yield ugrid.randomSVMData(N, D, seed).collect();
 
-	console.log(res)
-
 	assert(res.length == N);
 
-	for (var i = 0; i < N; i++) {
-		assert(res[i].label);
-		assert(res[i].features);
-		assert((res[i].label == 1) || (res[i].label == -1));
-		assert(res[i].features.length == D);
-	}
-
-	var rng = new ml.Random(seed);
-	var V = [];
-	for (i = 0; i < N; i++)
-		V.push({label: Math.round(Math.abs(rng.next())) * 2 - 1, features: rng.randn(D)});
-
-	// for (i = 0; i < V.length; i++) {
-	// 	assert(V[i].label == res[i].label);
-	// 	for (var j = 0; j < V[i].features.length; j++) {
-	// 		assert(V[i].features[j] == res[i].features[j]);
-	// 	}
-	// }
+	for (var i = 0; i < N; i++)
+		assert(res[i].length == (D + 1));
 
 	ugrid.end();
 })();
