8d0a4d83fd7386d4e6f5e747d235b54bb12d2116
Cédric Artigue <cedric.artigue@ugrid.net>
2015-02-18 16:15:00 +0100

    Added cogroup
    Former-commit-id: b68226e24bf6b47317c8c1acd19699e0346ef282

---
+++ test/unitTest/cogroup.js
@@ -0,0 +1,28 @@
+#!/usr/local/bin/node --harmony
+'use strict';
+
+// Join compute the cartesian product between a and b entries presenting same key
+// cogroup build a sequence of value for the same key
+// thus join output can present different entries with same key
+// which is not the case for cogroup
+
+var co = require('co');
+var assert = require('assert');
+var ugrid = require('../../lib/ugrid-context.js')();
+
+var a = [[0, 'hello'], [1, 'goodbye'], [1, 'TEST']];
+var b = [[0, 'cedric'], [1, 'marc']];
+
+co(function *() {
+	yield ugrid.init();
+
+	var p1 = ugrid.parallelize(a);
+	var p2 = ugrid.parallelize(b);
+	var p3 = p1.coGroup(p2);
+
+	// console.log(yield p1.collect());
+	// console.log(yield p2.collect());
+	console.log(yield p3.collect());	
+
+	ugrid.end();
+})();
