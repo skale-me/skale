9b8c31f3d4fcd98003766865e5facb07ea3a9c8c
Cédric Artigue <cedric.artigue@ugrid.net>
2015-02-19 15:47:21 +0100

    Added sample transformation
    Former-commit-id: 0600730dd8839b0b4030eb8f47871058429ef299

--- test/unitTest/sample.js
+++ test/unitTest/sample.js
@@ -1,27 +1,21 @@
 #!/usr/local/bin/node --harmony
+'use strict';
 
 var co = require('co');
 var assert = require('assert');
 var ugrid = require('../../lib/ugrid-context.js')();
+var ml = require('../../lib/ugrid-ml.js');
 
 co(function *() {
 	yield ugrid.init();
 
-	var N = 10;
-	var n = 2;
-	var frac = n / N;
+	var v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	var frac = 0.4;
 	var seed = 1;
-	var V = [];
 
-	for (var i = 0; i < N; i++)
-		V[i] = i;
+	var points = yield ugrid.parallelize(v).sample(frac, seed).collect();
 
-	var d1 = yield ugrid.parallelize(V).sample(frac, seed);
-
-	assert(d1.length == n)
-
-	for (i = 0; i < d1.length; i++)
-		assert(V.indexOf(d1[i]) != -1)
+	console.log(points)
 
 	ugrid.end();
 })();
