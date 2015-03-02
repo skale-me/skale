3456f87a246ad0683644ed0ba6d7bc3340ab7c8a
Marc Vertes <marc.vertes@ugrid.net>
2015-03-02 14:16:30 +0100

    ugrid_client: set websocket mode through env var UGRID_WS
    Former-commit-id: ef51e10e171dc5942a7355787c68a4bb8c45cd39

--- examples/monitor.js
+++ examples/monitor.js
@@ -1,7 +1,5 @@
 #!/usr/local/bin/node
 
-var grid = require('../lib/ugrid-client.js')({port: 12348, ws: true});
-
+var grid = require('../lib/ugrid-client.js')();
 grid.subscribe('monitoring');
-//grid.pipe('monitoring', process.stdout);
 grid.on('monitoring', console.log);
