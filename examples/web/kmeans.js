4d9265183f2e34632a1ef56d02a7708adf68be4e
Marc Vertes <marc.vertes@ugrid.net>
2015-04-02 12:44:09 +0200

    fix kmeans.js with new syntax
    Former-commit-id: 70445c507d764b3c244706f861d1067215adea5d

--- examples/web/kmeans.js
+++ examples/web/kmeans.js
@@ -3,8 +3,7 @@
 
 var co = require('co');
 
-var ugrid = require('../../../ugrid/lib/ugrid-context.js')({data: {type: 'master'}});
-var KMeans = require('../../../ugrid/lib/ugrid-ml.js').KMeans;
+var ugrid = require('../..');
 
 var args = JSON.parse(process.argv[2]);
 
@@ -13,10 +12,12 @@ var K = args.k;
 var ITERATIONS = args.it;
 
 co(function *() {
-	yield ugrid.init();
+	var uc = yield ugrid.context();
 
-	var ui = yield ugrid.grid.devices({type: 'webugrid'}, 0);
-	var viewer = ui[0].uuid;
+	var ui = yield uc.devices({type: 'webugrid'}, 0);
+	var viewer;
+	
+	if (ui[0]) viewer = ui[0].uuid;
 
 	var data = [];
 	var D = 2;
@@ -28,33 +29,30 @@ co(function *() {
 		data.push(t0);
 	}
 
-	var points = ugrid.parallelize(data).persist();
-	var model = new KMeans(points, K);
+	var points = uc.parallelize(data).persist();
+	var model = new ugrid.ml.KMeans(points, K);
 
 	// Send source data to UI
-	ugrid.grid.send(viewer, {cmd: 'kmeans', data: {
-		error: [],
-		points: yield points.map(function(e) {return [1, e]}).collect()
-	}});
+	if (viewer)
+		uc.send(viewer, {cmd: 'kmeans', data: {
+			error: [],
+			points: yield points.map(function(e) {return [1, e]}).collect()
+		}});
 
 	for (var i = 0; i < ITERATIONS; i++) {
 		yield model.train(1);
 
 		// Send clusterized data to UI
-		ugrid.grid.send(viewer, {cmd: 'kmeans', data: {
-			error: model.mse,
-			points: yield points.map(model.closestSpectralNorm, [model.means])
-				.map(function(e) {return [e[0] + 1, e[1].data]})
-				.collect()
-		}});
+		if (viewer)
+			uc.send(viewer, {cmd: 'kmeans', data: {
+				error: model.mse,
+				points: yield points.map(model.closestSpectralNorm, [model.means])
+					.map(function(e) {return [e[0] + 1, e[1].data]})
+					.collect()
+			}});
 	}
 
 	console.log('KMeans finished')
 
-	ugrid.end();
-}).catch(function (err) {
-	console.error(err.stack);
-	process.exit(1);
-});
-
-
+	uc.end();
+}).catch(ugrid.onError);
