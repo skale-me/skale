3fb72327b15ead4ea78cc94eebd8c43ad56e3c6c
Marc Vertes <marc.vertes@ugrid.net>
2015-04-07 11:22:45 +0200

    co is not needed in this program
    Former-commit-id: b12833f3706d55ac14e93e8dd0045e40036c566b

--- bin/controller.js
+++ bin/controller.js
@@ -2,7 +2,6 @@
 
 'use strict'
 
-var co = require('co');
 var spawn = require('child_process').spawn;
 
 var opt = require('node-getopt').create([
@@ -20,21 +19,16 @@ var ugrid = require('../lib/ugrid-client.js')({
 	data: {type: 'controller'}
 });
 
-co(function *() {
-	ugrid.on('start', function(res) {
-		var cmd = __dirname + '/../examples/web/' + res.data.app + '.js';
-		var prog = spawn('/usr/local/bin/node', ['--harmony', cmd, JSON.stringify(res.data)]);
-		prog.stdout.on('data', function(data) {
-			console.log('# stdout: ' + data);
-		});
-		prog.stderr.on('data', function(data) {
-			console.log('# stderr: ' + data);
-		});
-		prog.on('close', function (code) {
-		  	console.log('child process exited with code ' + code);
-		});		
+ugrid.on('start', function(res) {
+	var cmd = __dirname + '/../examples/web/' + res.data.app + '.js';
+	var prog = spawn('/usr/local/bin/node', ['--harmony', cmd, JSON.stringify(res.data)]);
+	prog.stdout.on('data', function(data) {
+		console.log('# stdout: ' + data);
 	});
-}).catch(function (err) {
-	console.error(err.stack);
-	process.exit(1);
+	prog.stderr.on('data', function(data) {
+		console.log('# stderr: ' + data);
+	});
+	prog.on('close', function (code) {
+		console.log('child process exited with code ' + code);
+	});
 });
