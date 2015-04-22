6d79046651512d9d19defdda486943c10485caf2
Marc Vertes <marc.vertes@ugrid.net>
2015-04-22 16:16:10 +0200

    Improve tests. Stop-all.sh if tests interrupted
    Former-commit-id: 2a8d5eab6525e70d25161f5bd5ec30674801d1f7

--- bin/start-all.sh
+++ bin/start-all.sh
@@ -32,25 +32,28 @@ host=${UGRID_HOST:-localhost} port=${UGRID_PORT:-12346} wph=${UGRID_WORKER_PER_H
 node=${NODE:-node} node_opts=${NODE_OPTS}
 
 # Start ugrid server
+printf "Starting ugrid server on $host port $port ... "
 ugrid_cmd="PATH=$PATH; [ -f $config ] && . $config; $node $node_opts $cpath/bin/ugrid.js >>/tmp/ugrid.log 2>&1 & echo \$! >/tmp/ugrid.pid"
 ssh $host "$ugrid_cmd"
 
 # Wait for ugrid server
-while ! $cpath/bin/wait-workers.js 0 2>/dev/null; do sleep 1; done
+while ! $cpath/bin/wait-workers.js 0 2>/dev/null; do sleep 1; done && echo "ok"
 
 # Start ugrid workers
-worker_cmd="{ set -x; PATH=$PATH; [ -f $config ] && . $config; $node $node_opts $cpath/bin/worker.js -H $host -P $port -n $wph; } >>/tmp/worker.log 2>&1 &"
+printf "Starting $wph workers on $host ... "
+worker_cmd="PATH=$PATH; [ -f $config ] && . $config; $node $node_opts $cpath/bin/worker.js -H $host -P $port -n $wph >>/tmp/worker.log 2>&1 &"
 set -- $workers
 for worker; do
 	ssh $worker "$worker_cmd"
 done
 
+# Wait for ugrid workers
+$cpath/bin/wait-workers.js $(($wph * $#)) && echo "ok"
+
 # Start ugrid controller
+printf "Starting controller on $host ... "
 controller_cmd="PATH=$PATH; [ -f $config ] && . $config; $node --harmony $node_opts $cpath/bin/controller.js -H $host -P $port >>/tmp/controller.log 2>&1 &"
 ssh $host "$controller_cmd"
 
-# Wait for ugrid workers
-$cpath/bin/wait-workers.js $(($wph * $#))
-
 # Wait for ugrid controller
-$cpath/bin/wait-controller.js
+$cpath/bin/wait-controller.js && echo "ok"
