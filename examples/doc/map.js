0921939ba11b634a5070cbc2f2465a7d05512e66
Cédric Artigue <cedric.artigue@ugrid.net>
2015-10-21 15:08:11 +0200

    Fixed bug on non stream action, fixed bug on logreg
    Former-commit-id: 17b6b473b9b314ded10ac7c45c72759108b887ee

--- examples/doc/map.js
+++ examples/doc/map.js
@@ -6,11 +6,9 @@ var ugrid = require('ugrid');
 ugrid.context(function(err, uc) {
 	if (err) {console.log(err); process.exit();}
 	
-	function mapper(data, obj) {
-		return data * obj.scaling;
-	}
-
-	var res = uc.parallelize([1, 2, 3, 4]).map(mapper, {scaling: 1.2}).collect();
+	var res = uc.parallelize([1, 2, 3, 4])
+		.map(function (data, args) {return data * args.scaling;}, {scaling: 1.2})
+		.collect();
 
 	res.on('data', console.log);
 	res.on('end', uc.end);
