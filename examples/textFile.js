53a83cc44edcdf099e0d478c80b120ccfbd17524
Cédric Artigue <cedric.artigue@ugrid.net>
2015-10-28 10:43:27 +0100

    Simplified and fixed issue on textFile
    Former-commit-id: a19fd035b8e8ec267d0a66af76dba3467743dc1c

--- examples/textFile.js
+++ examples/textFile.js
@@ -1,12 +1,31 @@
 #!/usr/local/bin/node
 'use strict';
 
+var fs = require('fs');
 var uc = new require('ugrid').Context();
 
-// var res = uc.textFile2("/Users/cedricartigue/work/ugrid/examples/less_than_a_line_with_4_workers").collect();
-// var res = uc.textFile2("/Users/cedricartigue/work/ugrid/examples/less_bytes_than_workers").collect();
 
-var res = uc.textFile("/Users/cedricartigue/work/ugrid/examples/random").collect();
+// var file = "/Users/cedricartigue/work/ugrid/examples/less_than_a_line_with_4_workers";
+// var file = "/Users/cedricartigue/work/ugrid/examples/less_bytes_than_workers";
+var file = "/Users/cedricartigue/work/ugrid/examples/random";
 
-res.on('data', console.log);
-res.on('end', uc.end);
+var expected = fs.readFileSync(file, {encoding: 'utf8'});
+
+var res = uc.textFile(file).collect();
+
+var str = '';
+res.on('data', function(data) {str += data + '\n'});
+res.on('end', function() {
+	str = str.substr(0, str.length - 1);
+	if (str == expected) console.log('TEST OK !!');
+	else {
+		console.log('TEST FAILED !!');
+		console.log('Expected : ')
+		console.log(expected)
+
+		console.log('\nresult: ')
+		console.log(str);
+	}
+	uc.end();
+});
+
