4a8f920784f27583bde5df37d3507ddcfd206c4f
Marc Vertes <marc.vertes@ugrid.net>
2016-01-27 15:04:55 +0100

    Implement parallelize() and range() by using Source()
    Former-commit-id: e92e554de094a1c1084950393d374f48e17f5d7c

--- examples/new/map.js
+++ examples/new/map.js
@@ -6,7 +6,7 @@ function by2(a, args) {return a * 2 * args.bias;}
 function sum(a, b) {return a + b;}
 
 var a = uc.parallelize([1, 2, 3, 4]).map(by2, {bias: 2}).reduce(sum, 0, function(err, res) {
-	console.log('Success !');
 	console.log(res);
+	console.assert(res == 40)
 	uc.end();
 })
