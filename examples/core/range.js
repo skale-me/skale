6b5bdf5c869deafebcf143fe868da05b3a5380f9
Marc Vertes <marc.vertes@ugrid.net>
2016-01-29 12:49:38 +0100

    Source(): do not allocate potentially big split array, only iterate on size.
    Simplify parallelize, range, randomSVMData()
    
    
    Former-commit-id: 70e6c83904db75abf88da9d8d47058e928b3617b

--- examples/core/range.js
+++ examples/core/range.js
@@ -2,13 +2,11 @@
 
 var sc = require('skale').context();
 
-//sc.range(10).map(a => a * 2).collect().toArray().then(console.log);
+sc.range(10).map(a => a * 2).collect().toArray().then(console.log);
 
-//sc.range(10, -5, -3).collect().toArray().then(console.log);
+sc.range(10, -5, -3).collect().toArray().then(console.log);
 
-//sc.range(-4, 3).map(function (a) {return a*2}).collect().toArray(function(err, res) {
-//sc.range(-4, 3).map(a => a*2).collect().toArray(function(err, res) {
-sc.range(4).map(a => a*2).reduce((a,b)=>a+b, 0, function(err, res) {
+sc.range(-4, 3).collect().toArray(function(err, res) {
 	console.log(res);
 	sc.end();
 })
