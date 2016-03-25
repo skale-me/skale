ce21f6b5c336be6744925962102ba6108a84b315
Marc Vertes <marc.vertes@ugrid.net>
2016-03-25 15:02:18 +0100

    Implement support of skale-engine on localhost

--- README.md
+++ README.md
@@ -1,44 +1,67 @@
-# skale-cli
+# skale
 
 [![Join the chat at https://gitter.im/skale-me/skale-cli](https://badges.gitter.im/skale-me/skale-cli.svg)](https://gitter.im/skale-me/skale-cli?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
-[![Build
-Status](https://travis-ci.org/skale-me/skale-cli.svg?branch=master)](https://travis-ci.org/skale-me/skale-cli)
+[![Build Status](https://travis-ci.org/skale-me/skale-cli.svg?branch=master)](https://travis-ci.org/skale-me/skale-cli)
 
-Command line interface tool for skale.
+Create, run and deploy distributed NodeJS applications
+
+The skale command is the fatest and easiest way to create distributed
+scalable applications based on
+[skale-engine](https://www.npmjs.com/package/skale-engine) and then
+to submit them either locally or on the cloud.
 
 ## Install
-```
-$ npm install -g skale
-```
+
+Assuming you have already installed [NodeJS](https://nodejs.org):
+
+	$ npm install -g skale
+
+## Getting started
+
+To create an application called `my_app`, simply run
+
+	$ skale create my_app
+
+It will create a directory `my_app`, populate it with a template application
+`my_app.js`, and install skale-engine dependency using [`npm`](https://npmjs.com).
+
+You can then go to `my_app` and run your app using `skale run`:
+
+	$ cd my_app
+	$ skale run
+
+To do something useful, modify your application by editing `my_app.js`,
+then run again using `skale run`.
+
+Under the hood, the `skale run` commands starts a skale-engine cluster on the
+the localhost at the first invocation, then simply executes `my_app.js`. You
+can check the status of the cluster using the `skale status` command, and stop
+it using `skale stop`.
+
+To go on further details, please refer to skale-engine documentation.
+ 
+## Deploying an application
+
+*Coming soon*
 
 ## Usage
 ```
 Usage: skale [options] <command> [<args>]
 
+Create, run, deploy clustered node applications
+
 Commands:
-  run <file> [<args>...]	Run file on skale cluster
+  create <app>          Create a new application
+  run [<args>...]       Run application
+  deploy [<args>...]    Deploy application (coming soon)
+  status                print status of local skale cluster
+  stop                  Stop local skale cluster
 
 Options:
-  -H, --host=<hostname> Set skale hostname
-  -p, --port=<portnum>  Set skale port number
-  -k, --key=<acess_key> Set skale access key
-  -s, --ssl             Use SSL protocol
   -h, --help            Show help
   -V, --version         Show version
 ```
 
 ## License
 
-Copyright 2016 Luca-SAS
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this program except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+[Apache-2.0](LICENSE)
--- skale.js
+++ skale.js
@@ -4,27 +4,30 @@
 
 const help=`Usage: skale [options] <command> [<args>]
 
+Create, run, deploy clustered node applications
+
 Commands:
-  init				Set configuration
-  run <file> [<args>...]	Run file on skale cluster
-  demo <file> [<args>...]	Run demo file on skale cluster
+  create <app>		Create a new application
+  run [<args>...]	Run application
+  deploy [<args>...]	Deploy application (coming soon)
+  status		print status of local skale cluster
+  stop			Stop local skale cluster
 
 Options:
-  -c, --config=<file>	Set configuration file	[~/.skalerc]
-  -H, --host=<hostname>	Set skale hostname	[SKALE_HOST] 
-  -p, --port=<portnum>	Set skale port number	[SKALE_PORT]
-  -k, --key=<acess_key>	Set skale access key	[SKALE_KEY]
-  -s, --ssl		Use SSL protocol
   -h, --help		Show help
   -V, --version		Show version
 `;
 
+const child_process = require('child_process');
 const fs = require('fs');
+const net = require('net');
 
 const argv = require('minimist')(process.argv.slice(2), {
 	string: ['c', 'config', 'H', 'host', 'p', 'port', 'k', 'key'],
 	boolean: ['h', 'help', 'V', 'version', 's', 'ssl'],
 });
+
+var skale_port = 12346;
 
 if (argv.h || argv.help) {
 	console.log(help);
@@ -40,16 +43,59 @@ const config = load(argv);
 const proto = config.ssl ? require('https') : require('http');
 
 switch (argv._[0]) {
-	case 'init':
+	case 'create':
+		create(argv._[1]);
 		break;
 	case 'demo':
-		run(__dirname + '/demo/' + argv._[1], argv._.splice(2));
+		run_remote(__dirname + '/demo/' + argv._[1], argv._.splice(2));
 		break;
 	case 'run':
-		run(argv._[1], argv._.splice(2));
+		run_local(argv._.splice(1));
+		break;
+	case 'status':
+		status_local();
+		break;
+	case 'stop':
+		stop_local_server();
 		break;
 	default:
 		die('Error: invalid command: ' + argv._[0]);
+}
+
+function create(name) {
+	console.log('create application ' + name);
+	try {
+		fs.mkdirSync(name);
+	} catch (error) {
+		die('skale create error: ' + error.message);
+	}
+	process.chdir(name);
+	const pkg = {
+		name: name,
+		version: '0.0.0',
+		private: true,
+		keywords: [ 'skale' ],
+		dependencies: {
+			'skale-engine': '^0.3.2'
+		}
+	};
+	fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));
+	var src = `#!/usr/bin/env node
+
+var sc = require('skale-engine').context();
+
+sc.parallelize(['Hello world'])
+  .collect()
+  .on('data', console.log)
+  .on('end', sc.end);
+`;
+	fs.writeFileSync(name + '.js', src);
+	const npm = child_process.spawnSync('npm', ['install'], {stdio: 'inherit'});
+	if (npm.status) die('skale create error: npm install failed');
+	console.log(`Project ${name} is now ready.
+Pleas change directory to ${name}: "cd ${name}"
+To run your app: "skale run"
+To modify your app: edit ${name}.js`)
 }
 
 function die(err) {
@@ -70,7 +116,56 @@ function load(argv) {
 	return conf;
 }
 
-function run(src, args) {
+function start_skale(done) {
+	const out = fs.openSync('skale-server.log', 'a');
+	const err = fs.openSync('skale-server.log', 'a');
+	const child = child_process.spawn('node_modules/skale-engine/bin/server.js', ['-l', '2'], {
+		detached: true,
+		stdio: ['ignore', out, err]
+	});
+	child.unref();
+	try_connect(5, 1000, done);
+}
+
+function try_connect(nb_try, timeout, done) {
+	const sock = net.connect(skale_port);
+	sock.on('connect', function () {
+		sock.end();
+		done(null);
+	});
+	sock.on('error', function (err) {
+		if (--nb_try <= 0) return done('skale-server not ok');
+		setTimeout(function () { try_connect(nb_try, timeout, done); }, timeout);
+	});
+}
+
+function stop_local_server() {
+	const child = child_process.execFile('/usr/bin/pgrep', ['skale-server'], function (err, pid) {
+		if (! pid) return;
+		process.kill(pid.trim());
+	});
+}
+
+function status_local() {
+	const child = child_process.execFile('/bin/ps', ['ux'], function (err, out) {
+		var lines = out.split(/\r\n|\r|\n/);
+		for (var i = 0; i < lines.length; i++)
+			if (i == 0 || lines[i].match(/ skale-/)) console.log(lines[i]);
+	});
+}
+
+function run_local(args) {
+	const pkg = JSON.parse(fs.readFileSync('package.json'));
+	var cmd = pkg.name + '.js';
+	args.splice(0, 0, cmd);
+	try_connect(0, 0, function (err) {
+		if (!err) return run_app();
+		start_skale(run_app);
+	});
+	function run_app() { child = child_process.spawn('node', args, {stdio: 'inherit'}); }
+}
+
+function run_remote(src, args) {
 	fs.readFile(src, {encoding: 'utf8'}, function (err, data) {
 		if (err) throw err;
 
