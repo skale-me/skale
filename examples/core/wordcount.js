a481a8d873a8323f80c4080b1c3e29478963e9c1
Marc Vertes <marc.vertes@ugrid.net>
2016-03-31 11:20:32 +0200

    Add sc.end() to exit after results. Suggested by @tfauck.

--- examples/core/wordcount.js
+++ examples/core/wordcount.js
@@ -8,4 +8,7 @@ sc.textFile(file)
   .flatMap(line => line.split(' '))
   .map(word => [word, 1])
   .reduceByKey((a, b) => a + b, 0)
-  .count().then(console.log)
+  .count().then(function (res) {
+  	console.log(res);
+	sc.end();
+  });
