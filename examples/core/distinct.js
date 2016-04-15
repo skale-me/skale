5804a6a2e9e916c1a132957e9a31305293cb9473
Marc Vertes <mvertes@free.fr>
2016-04-15 14:45:38 +0200

    Simplify code, fix lint errors. More to come.

--- examples/core/distinct.js
+++ examples/core/distinct.js
@@ -6,6 +6,7 @@ sc.parallelize([ 1, 2, 3, 1, 4, 3, 5 ]).
   distinct().
   collect().toArray(function(err, res) {
 	console.log(res);
-	console.assert(JSON.stringify(res) === JSON.stringify([4, 1, 5, 2, 3]));
+	res.sort();
+	console.assert(JSON.stringify(res) === JSON.stringify([1, 2, 3, 4, 5]));
 	sc.end();
 })
