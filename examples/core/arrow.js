72dae7800119895bbee3acc213b08e4d08e87796
Marc Vertes <mvertes@free.fr>
2016-04-24 13:48:24 +0200

    fix broken arrow function example

--- examples/core/arrow.js
+++ examples/core/arrow.js
@@ -2,7 +2,8 @@
 
 var sc = require('skale-engine').context();
 
-sc.range(6).map(a => a*a).reduce((a,b) => a+b, 0).then(function (res) {
+sc.range(6).map(a => a*a).reduce((a,b) => a+b, 0)
+  .on('data', function (res) {
 	console.log(res);
 	console.assert(res == 55);
 	sc.end();
