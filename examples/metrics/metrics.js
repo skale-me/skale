e0d7f8071d009c17cc20f05b0954c2917a64a135
Cédric Artigue <cedric.artigue@ugrid.net>
2016-04-28 15:58:48 +0200

    Improved lib modularity and adult example

--- examples/metrics/metrics.js
+++ examples/metrics/metrics.js
@@ -1,17 +1,9 @@
 #!/usr/bin/env node
 
-/*
-	In this example we:
-	- train a logistic regression on the adult training set.
-	- evaluate the model on the adult test set
-	- generate ROC curves as png images
-*/
-
 // Adult dataset processing as per http://scg.sdsu.edu/dataset-adult_r/
 var co = require('co');
 var sc = require('skale-engine').context();
 var ml = require('../../index.js');								// should be require('skale-ml');
-var plot = require('plotter').plot;
 
 var LogisticRegressionWithSGD = ml.LogisticRegressionWithSGD;
 var StandardScaler = ml.StandardScaler;
@@ -60,50 +52,78 @@ var test_set = sc.textFile('adult.test')
 	.filter(data => data.indexOf('?') == -1)					// remove incomplete data
 	.map(featurize, metadata);									// transform string data to number
 
-// Wrap asynchronous code in co to limit callback imbrications (ES7 async/await will fix this !)
+
+// Co is workaround until async/await is available
 co(function* () {
 	// Standardize features to zero mean and unit variance
+	var features = training_set.map(point => point[1]);	
 	var scaler = new StandardScaler();
 
-	yield scaler.fit(training_set.map(point => point[1]));			// async !!
+	yield scaler.fit(features);			// async
 
-	// Use scaler to standardize training and test datasets
-	var training_set_std = training_set
-		.map((p, args) => [p[0], args.scaler.transform(p[1])], {scaler: scaler})
-		.persist();
+	function standardize(point, args) {
+		var label = point[0], features = point[1], features_std = [];
 
-	var test_set_std = test_set
-		.map((p, args) => [p[0], args.scaler.transform(p[1])], {scaler: scaler});
+		for (var i in features)
+			features_std[i] = (features[i] - args.mean[i]) / args.std[i];
+
+		return [label, features_std];
+	}
+
+	// Transform features into standardized features
+	var training_set_std = training_set.map(standardize, {mean: scaler.mean, std: scaler.std}).persist();
+	var test_set_std = test_set.map(standardize, {mean: scaler.mean, std: scaler.std});
 
 	// Train logistic regression with SGD on standardized training set
-	var nIterations = 10;
-	var parameters = {regParam: 0.01, stepSize: 1};
-	var model = new LogisticRegressionWithSGD(training_set_std, parameters);
+	var model = new LogisticRegressionWithSGD(training_set_std, {regParam: 0.01, stepSize: 1});
+	var nIterations = 100;
 
-	yield model.train(nIterations);									// async !!
+	yield model.train(nIterations);		// async
 
 	// Evaluate classifier performance on standardized test set
-	var predictionAndLabels = test_set_std.map((p, args) => [args.model.predict(p[1]), p[0]], {model: model});
+	var predictionAndLabels = test_set_std.map(function(point, args) {
+		var margin = 0, label = point[0], features = point[1];
+
+		for (var i = 0; i < features.length; i++)
+			margin += args.model.weights[i] * features[i];
+		var prediction = 1 / (1 + Math.exp(-margin));
+
+		return [prediction, label];
+	}, {model: model});
+
 	var metrics = new BinaryClassificationMetrics(predictionAndLabels);
+
+	console.log('\n# Precision by threshold');
+	var precision = yield metrics.precisionByThreshold();
+	precision.forEach(p => console.log("Threshold: %d, Precision: %d", p[0].toFixed(2), p[1].toFixed(2)));
+
+	console.log('\n# Recall by threshold');
+	var recall = yield metrics.recallByThreshold();
+	recall.forEach(p => console.log("Threshold: %d, Recall: %d", p[0].toFixed(2), p[1].toFixed(2)));
+
+	console.log('\n# Precision-recall curve: TODO !!');
+	// var prc = yield metrics.pr();
+
+	console.log('\n# F1-score by threshold');
+	var beta = 1;
+	var f1Score = yield metrics.fMeasureByThreshold(1);
+	f1Score.forEach(p => console.log("Threshold: %d, F-score: %d", p[0].toFixed(2), p[1].toFixed(2), beta));
+
+	console.log('\n# Area under precision-recall curve: TODO !!')
 
 	console.log('\n# Receiver Operating characteristic (ROC)')
 	var roc = yield metrics.roc();
 	console.log('\nThreshold\tSpecificity(FPR)\tSensitivity(TPR)')
 	for (var i in roc)
-		console.log(roc[i][0].toFixed(2) + '\t' + roc[i][1][0].toFixed(2) + '\t' + roc[i][1][1].toFixed(2));
+		console.log(roc[i][0].toFixed(2).replace('.', ',') + '\t' + roc[i][1][0].toFixed(2).replace('.', ',') + '\t' + roc[i][1][1].toFixed(2).replace('.', ','));
 
-	// Ploting ROC curve as roc.png
-	var xy = {};
-	for (var i in roc)
-		xy[roc[i][1][0].toFixed(2)] = roc[i][1][1].toFixed(2)
-	xy['0.00'] = '0.00';
-	var data = {};
-	data['regParam: ' + parameters.regParam + ', stepSize: ' + parameters.stepSize] = xy;
-	data['Random'] = {0 :0, 1 : 1};
-	plot({
-		title: 'Logistic Regression ROC Curve', 
-		data: data, 
-		filename: 'roc.png',
-		finish: function() {sc.end();}
-	});
+	console.log('\n# Area under ROC curve: TODO !!')
+
+	console.log('\n# Accuracy by threshold')
+	var accuracy = yield metrics.accuracyByThreshold();
+	console.log('\nThreshold\tAccuracy')
+	for (var i in accuracy)
+		console.log(accuracy[i][0].toFixed(2).replace('.', ',') + '\t' + accuracy[i][1].toFixed(2).replace('.', ','));
+
+	sc.end();
 });
