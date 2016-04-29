6845aa21d27b61218aaa44c55bf3060052b477a4
Cédric Artigue <cedric.artigue@ugrid.net>
2016-04-29 09:32:48 +0200

    Restaured logreg sample app

---
+++ examples/logreg/logreg.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+var skale = require('skale-engine');
+var ml = require('skale-ml');
+
+var sc = skale.context();
+
+var nObservations = 10;
+var nFeatures = 16;
+var nIterations = 10;
+var seed = 1
+
+var points = ml.randomSVMData(sc, nObservations, nFeatures, seed).persist();
+
+var model = new ml.LogisticRegressionWithSGD(points);
+
+model.train(nIterations, function() {
+    console.log(model.weights);
+    sc.end();
+});
