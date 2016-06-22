8779d889f5f1d64339885fa9c29d7431eb835945
Marc Vertes <mvertes@free.fr>
2016-06-22 12:48:58 +0200

    fix previous

--- examples/save.js
+++ examples/save.js
@@ -2,7 +2,7 @@
 
 var sc = require('skale-engine').context();
 
-sc.range(5).map(a => 2*a).save('/tmp/truc', function (err, res) {
+sc.range(5).map(a => 2*a).save('/tmp/truc', function (err) {
 	if (err) console.log(err);
 	sc.end();
 });
