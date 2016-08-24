35608a72f59d43b3ff12898b87e912ec730b0d0d
Marc Vertes <mvertes@free.fr>
2016-08-24 13:42:08 +0200

    fix eslint

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ var ContextLocal = require('./lib/context-local.js');
 var Dataset = require('./lib/dataset.js');
 
 function Context(args) {
-	args = args || {};
-	if (args.host || process.env.SKALE_HOST) return ContextRemote(args);
-	return ContextLocal(args);
+  args = args || {};
+  if (args.host || process.env.SKALE_HOST) return ContextRemote(args);
+  return ContextLocal(args);
 }
 
 module.exports = {
-	Context: Context,
-	context: Context,
-	HashPartitioner: Dataset.HashPartitioner,
-	RangePartitioner: Dataset.RangePartitioner,
-	Random: Dataset.Random,
-	Source: Dataset.Source
+  Context: Context,
+  context: Context,
+  HashPartitioner: Dataset.HashPartitioner,
+  RangePartitioner: Dataset.RangePartitioner,
+  Random: Dataset.Random,
+  Source: Dataset.Source
 };
