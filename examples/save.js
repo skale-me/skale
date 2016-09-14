e615cdd18a2f8255a6810e56f1e7a169449916a7
Marc Vertes <mvertes@free.fr>
2016-09-14 15:50:18 +0200

    fix lint errors

--- examples/save.js
+++ examples/save.js
@@ -6,4 +6,4 @@ sc.range(900).save('/tmp/truc', {gzip: true}, (err, res) => {
 // sc.range(900).save('s3://skale-demo/test/s1', {gzip: true}, (err, res) => {
   console.log(res);
   sc.end();
-})
+});
