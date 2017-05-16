df3038aa77ca213ee12416a9f8683fbd8833ab5f
Marc Vertes <mvertes@free.fr>
2017-05-16 16:54:28 +0200

    Fix a worker crash when using ds.sample() with replacement.
    Also use again Math.random(), and allow to set a random seed using
    the external seedrandom module. Use env SKALE_RANDOM_SEED to
    set a seed. Setting a seed induce a performance penalty, as it
    uses a pure JS random function instead of the native one.

--- examples/sample.js
+++ examples/sample.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
+process.env.SKALE_RANDOM_SEED = 'skale';
+
 var sc = require('skale-engine').context();
 
 sc.parallelize([1, 2, 3, 4])
   .sample(false, 0.8)
   .collect(function(err, res) {
     console.log(res);
-    console.assert(JSON.stringify(res) === JSON.stringify([1, 2, 3, 4])); 
+    console.assert(JSON.stringify(res) === JSON.stringify([1, 2, 4]));
     sc.end();
   });
