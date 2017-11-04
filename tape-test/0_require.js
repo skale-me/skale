863d1606c044567f06f3ede55a33d942d52275b7
Marc Vertes <mvertes@free.fr>
2017-11-04 15:37:55 +0100

    Implement dependency injection into workers, resolves #203
    A new skale context method `sc.require` is added. It specifies a
    set of modules on user side (master) which have to be deployed in
    workers for use by callbacks, such as mappers, reducers, etc.
    
    Under the hood, [browserify] is used on master side to build a
    bundle which is serialized and sent to workers (as part of task).
    It is then evaluated in worker global context, and dependencies
    remain persistent as long as workers live.
    
    This method allows to use in workers any javascript module which
    can be browserified, so a large number (almost any pure JS package).
    
    The current commit is for local version, not distributed (code will
    be exactly the same). It is experimental for the moment.
    
    An additional statement `sc.bundle` could be added as well to inject
    pre-compiled modules, avoiding the penalty of `browserify` at each
    run.
    
    [browserify]: https://github.com/browserify/browserify

---
+++ tape-test/0_require.js
@@ -0,0 +1,14 @@
+var t = require('tape');
+var sc = require('skale-engine').context();
+t.onFinish(sc.end);
+
+t.test('require', function (t) {
+  t.plan(1);
+
+  sc.require({add3: './dep'})
+    .range(4)
+    .map(a => add3(a))                // eslint-disable-line no-undef
+    .collect(function (err, res) {
+      t.deepEquals(res, [3, 4, 5, 6]);
+    });
+});
