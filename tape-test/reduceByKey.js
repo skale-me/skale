27a97e977134d82a6d38cf73414034600854013a
Marc Vertes <mvertes@free.fr>
2017-11-06 14:33:28 +0100

    additional tests, as per #205

---
+++ tape-test/reduceByKey.js
@@ -0,0 +1,20 @@
+var t = require('tape');
+var sc = require('skale-engine').context();
+
+var data = [['hello', 1], ['hello', 1], ['world', 1]];
+var nPartitions = 2;
+
+var init = 0;
+
+function reducer(a, b) {return a + b;}
+
+t.test('reduceByKey', function (t) {
+  t.plan(1);
+
+  sc.parallelize(data, nPartitions)
+    .reduceByKey(reducer, init)
+    .collect(function(err, res) {
+      t.deepEqual(res.sort(), [['hello', 2], ['world', 1]]);
+      sc.end();
+    });
+});
