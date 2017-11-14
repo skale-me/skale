0f89c715db020709297e30be5d503a00a943fce4
Marc Vertes <mvertes@free.fr>
2017-11-14 15:13:31 +0100

    put old/ in attic

--- old/examples/core/arrow.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var uc = require('ugrid').context();
-
-uc.range(6).map(a => a*a).reduce((a,b) => a+b, 0).then(function (res) {
-	console.log(res);
-	console.assert(res == 55);
-	uc.end();
-});
