a3a91dce91e9471ddb1d9cd067ff8c3b320cbcea
Marc Vertes <mvertes@free.fr>
2017-11-17 14:24:51 +0100

    test textfile: add gzipped and directories

--- test/textFile.js
+++ test/textFile.js
@@ -1,12 +1,41 @@
 const t = require('tape');
 const sc = require('skale').context();
 
-const file = __dirname + '/data/iris.csv';
-
-t.test('textFile', function (t) {
+t.test('textFile local file', function (t) {
   t.plan(1);
+  sc.textFile(__dirname + '/data/iris.csv')
+    .count(function (err, res) {
+      t.ok(res === 151);
+    });
+});
 
-  sc.textFile(file)
+t.test('textFile compressed file', function (t) {
+  t.plan(1);
+  sc.textFile(__dirname + '/data/iris.csv.gz')
+    .count(function (err, res) {
+      t.ok(res === 151);
+    });
+});
+
+t.test('textFile dir', function (t) {
+  t.plan(1);
+  sc.textFile(__dirname + '/data/split/')
+    .count(function (err, res) {
+      t.ok(res === 151);
+    });
+});
+
+t.test('textFile compressed dir', function (t) {
+  t.plan(1);
+  sc.textFile(__dirname + '/data/split-gz/')
+    .count(function (err, res) {
+      t.ok(res === 151);
+    });
+});
+
+t.test('textFile multiple files', function (t) {
+  t.plan(1);
+  sc.textFile(__dirname + '/data/split/iris-*.csv')
     .count(function (err, res) {
       t.ok(res === 151);
       sc.end();
