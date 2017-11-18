e89812d6babba525302729db724fbc582f7f1eba
Marc Vertes <mvertes@free.fr>
2017-11-18 15:08:20 +0100

    textFile S3: skip test if no AWS key defined

--- test/textFile-s3.js
+++ test/textFile-s3.js
@@ -2,9 +2,11 @@ const t = require('tape');
 const sc = require('skale').context();
 
 console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
+const skip = process.env.AWS_ACCESS_KEY_ID ? false : true;
 
 t.test('textFile s3 file', function (t) {
   t.plan(1);
+  if (skip) return t.pass('SKIP # no aws access key');
   sc.textFile('s3://skale-test-eu-west-1/test/iris.csv')
     .count(function (err, res) {
       t.ok(res === 151);
@@ -13,6 +15,7 @@ t.test('textFile s3 file', function (t) {
 
 t.test('textFile s3 compressed file', function (t) {
   t.plan(1);
+  if (skip) return t.pass('SKIP # no aws access key');
   sc.textFile('s3://skale-test-eu-west-1/test/iris.csv.gz')
     .count(function (err, res) {
       t.ok(res === 151);
@@ -21,6 +24,7 @@ t.test('textFile s3 compressed file', function (t) {
 
 t.test('textFile s3 dir', function (t) {
   t.plan(1);
+  if (skip) return t.pass('SKIP # no aws access key');
   sc.textFile('s3://skale-test-eu-west-1/test/split/')
     .count(function (err, res) {
       t.ok(res === 151);
@@ -29,6 +33,7 @@ t.test('textFile s3 dir', function (t) {
 
 t.test('textFile s3 compressed dir', function (t) {
   t.plan(1);
+  if (skip) return t.pass('SKIP # no aws access key');
   sc.textFile('s3://skale-test-eu-west-1/test/split-gz/')
     .count(function (err, res) {
       t.ok(res === 151);
@@ -37,6 +42,10 @@ t.test('textFile s3 compressed dir', function (t) {
 
 t.test('textFile s3 multiple files', function (t) {
   t.plan(1);
+  if (skip) {
+    sc.end();
+    return t.pass('SKIP # no aws access key');
+  }
   sc.textFile('s3://skale-test-eu-west-1/test/split/iris-*.csv')
     .count(function (err, res) {
       t.ok(res === 151);
