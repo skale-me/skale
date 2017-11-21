c45fc1365594ff3f6c36f7cb94f1d7de17d6fdea
Marc Vertes <mvertes@free.fr>
2017-11-21 16:25:26 +0100

    Added test for save to S3. Use verbose assertions

--- test/save.js
+++ test/save.js
@@ -8,32 +8,36 @@ const savedir = '/tmp/skale-test/save';
 t.test('save', function (t) {
   t.plan(3);
 
-  rimraf.sync(savedir);
-  sc.range(10)
-    .save('/tmp/skale-test/save', function (err) {
-      t.ok(!err);
-      t.ok(fs.existsSync(savedir + '/0'));
-      sc.textFile(savedir + '/')
-        .map(a => JSON.parse(a))
-        .collect(function (err, res) {
-          t.deepEqual(res, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        });
+  rimraf(savedir, function (err) {
+    t.ok(!err, 'delete previous saved data');
+    sc.range(10)
+      .save(savedir, function (err) {
+        t.ok(!err, 'save returns no error');
+        t.ok(fs.existsSync(savedir + '/0'), 'saved filename is correct');
+        sc.textFile(savedir + '/')
+          .map(a => JSON.parse(a))
+          .collect(function (err, res) {
+            t.deepEqual(res, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 'saved content is correct');
+          });
+      });
     });
 });
 
-t.test('save', function (t) {
-  t.plan(3);
+t.test('save gzip', function (t) {
+  t.plan(4);
 
-  rimraf.sync(savedir);
-  sc.range(10)
-    .save('/tmp/skale-test/save', {gzip: true}, function (err) {
-      t.ok(!err);
-      t.ok(fs.existsSync(savedir + '/0.gz'));
-      sc.textFile(savedir + '/')
-        .map(a => JSON.parse(a))
-        .collect(function (err, res) {
-          t.deepEqual(res, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-          sc.end();
-        });
+  rimraf(savedir, function (err) {
+    t.ok(!err, 'delete previous saved data');
+    sc.range(10)
+      .save(savedir, {gzip: true}, function (err) {
+        t.ok(!err, 'save returns no error');
+        t.ok(fs.existsSync(savedir + '/0.gz'), 'saved filename is correct');
+        sc.textFile(savedir + '/')
+          .map(a => JSON.parse(a))
+          .collect(function (err, res) {
+            t.deepEqual(res, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 'saved content is correct');
+            sc.end();
+          });
+      });
     });
 });
