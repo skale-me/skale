1d4a634c52e41c84d6ba89fef88843a88dadc10b
Marc Vertes <mvertes@free.fr>
2017-11-21 23:23:06 +0100

    improve test skip

--- test/textFile-s3.js
+++ test/textFile-s3.js
@@ -3,51 +3,45 @@ const sc = require('skale').context();
 
 const skip = process.env.AWS_ACCESS_KEY_ID ? false : true;
 
-t.test('textFile s3 file', function (t) {
+t.test('textFile s3 file', {skip: skip}, function (t) {
   t.plan(1);
-  if (skip) return t.pass('SKIP # no aws access key');
   sc.textFile('s3://skale-test-eu-west-1/test/iris.csv')
     .count(function (err, res) {
       t.ok(res === 151);
     });
 });
 
-t.test('textFile s3 compressed file', function (t) {
+t.test('textFile s3 compressed file', {skip: skip}, function (t) {
   t.plan(1);
-  if (skip) return t.pass('SKIP # no aws access key');
   sc.textFile('s3://skale-test-eu-west-1/test/iris.csv.gz')
     .count(function (err, res) {
       t.ok(res === 151);
     });
 });
 
-t.test('textFile s3 dir', function (t) {
+t.test('textFile s3 dir', {skip: skip}, function (t) {
   t.plan(1);
-  if (skip) return t.pass('SKIP # no aws access key');
   sc.textFile('s3://skale-test-eu-west-1/test/split/')
     .count(function (err, res) {
       t.ok(res === 151);
     });
 });
 
-t.test('textFile s3 compressed dir', function (t) {
+t.test('textFile s3 compressed dir', {skip: skip}, function (t) {
   t.plan(1);
-  if (skip) return t.pass('SKIP # no aws access key');
   sc.textFile('s3://skale-test-eu-west-1/test/split-gz/')
     .count(function (err, res) {
       t.ok(res === 151);
     });
 });
 
-t.test('textFile s3 multiple files', function (t) {
+t.test('textFile s3 multiple files', {skip: skip}, function (t) {
   t.plan(1);
-  if (skip) {
-    sc.end();
-    return t.pass('SKIP # no aws access key');
-  }
   sc.textFile('s3://skale-test-eu-west-1/test/split/iris-*.csv')
     .count(function (err, res) {
       t.ok(res === 151);
       sc.end();
     });
 });
+
+if (skip) sc.end();
