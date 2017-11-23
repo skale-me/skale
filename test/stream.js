b3264d018fc5285a863b92828fec14aae093dd84
Marc Vertes <mvertes@free.fr>
2017-11-23 18:28:09 +0100

    test gzipped stream action

--- test/stream.js
+++ test/stream.js
@@ -1,4 +1,5 @@
 const stream = require('stream');
+const zlib =  require('zlib');
 const t = require('tape');
 const sc = require('skale').context();
 
@@ -8,11 +9,27 @@ t.test('stream', function (t) {
   let res = '';
   const s = sc.range(10).stream();
 
-  t.ok(s instanceof stream.Readable);
+  t.ok(s instanceof stream.Readable, 'ds.stream() returns a readable stream');
 
-  s.on('data', function (data) {res += data.toString();}); 
+  s.on('data', function (data) {res += data.toString();});
   s.on('end', function () {
-    t.equal(res, '0\n1\n2\n3\n4\n5\n6\n7\n8\n\9\n');
+    t.equal(res, '0\n1\n2\n3\n4\n5\n6\n7\n8\n\9\n', 'data read is correct');
+  });
+});
+
+t.test('stream gzip', function (t) {
+  t.plan(2);
+
+  let res = '';
+  const s = sc.range(10).stream({gzip: true});
+
+  t.ok(s instanceof stream.Readable, 'ds.stream() returns a readable stream');
+
+  const rs = s.pipe(zlib.createGunzip());
+
+  rs.on('data', function (data) {res += data.toString();});
+  rs.on('end', function () {
+    t.equal(res, '0\n1\n2\n3\n4\n5\n6\n7\n8\n\9\n', 'gunzip data read is correct');
     sc.end();
   });
 });
