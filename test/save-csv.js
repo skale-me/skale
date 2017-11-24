c86cc10efabd4380f278cfc8ee0cfa67f914c087
Marc Vertes <mvertes@free.fr>
2017-11-24 16:04:59 +0100

    fix and test save() to CSV format (#241)

--- test/save-csv.js
+++ test/save-csv.js
@@ -17,32 +17,12 @@ t.test('save csv', function (t) {
         t.ok(fs.existsSync(savedir + '/0.csv'), 'saved filename is correct');
         sc.textFile(savedir + '/')
           .collect(function (err, res) {
-            //t.deepEqual(res, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 'saved content is correct');
-            console.log('res:', res);
-            t.pass();
+            t.deepEqual(res, [
+              '0;0;0', '1;1;1', '2;2;2', '3;3;3', '4;4;4',
+              '5;5;5', '6;6;6', '7;7;7', '8;8;8', '9;9;9'
+            ], 'saved content is correct');
             sc.end();
           });
       });
   });
 });
-
-/*
-t.test('save gzip', function (t) {
-  t.plan(4);
-
-  rimraf(savedir, function (err) {
-    t.ok(!err, 'delete previous saved data');
-    sc.range(10)
-      .save(savedir, {gzip: true}, function (err) {
-        t.ok(!err, 'save returns no error');
-        t.ok(fs.existsSync(savedir + '/0.gz'), 'saved filename is correct');
-        sc.textFile(savedir + '/')
-          .map(a => JSON.parse(a))
-          .collect(function (err, res) {
-            t.deepEqual(res, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 'saved content is correct');
-            sc.end();
-          });
-      });
-  });
-});
-*/
